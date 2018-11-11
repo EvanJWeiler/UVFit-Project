7a4def8759f70f2d4b443b57b4d17ad531901865
EvanJWeiler <evanjweiler@gmail.com>
2018-11-11 01:16:10 -0700

    webhook test

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,11 +2,20 @@
 
 const express = require('express');
 const path = require('path');
+const exec = require('child_process').exec;
 
 const router = express.Router();
 
 router.get('/', function (req, res) {
   res.sendFile(path.resolve('./public/index.html'));
+});
+
+router.post('/', function (req, res) {
+  exec('echo test 123', function (error, stdout, stderr) {
+    console.log(error);
+    console.log(stdout);
+    console.log(stderr);
+  });
 });
 
 module.exports = router;
