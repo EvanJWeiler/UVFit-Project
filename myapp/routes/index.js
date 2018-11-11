4cf69142054961e3e5a98a2d74574a6ac43ab258
EvanJWeiler <evanjweiler@gmail.com>
2018-11-11 02:02:48 -0700

    push

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -10,12 +10,12 @@ router.get('/', function (req, res) {
   res.sendFile(path.resolve('./public/index.html'));
 });
 
-// router.post('/', function (req, res) {
-//   exec('./restart.sh', function (error, stdout, stderr) {
-//     console.log(error);
-//     console.log(stdout);
-//     console.log(stderr);
-//   });
-// });
+router.post('/', function (req, res) {
+  exec('./restart.sh', function (error, stdout, stderr) {
+    console.log(error);
+    console.log(stdout);
+    console.log(stderr);
+  });
+});
 
 module.exports = router;
