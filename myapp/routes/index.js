ec1e17de3b82f3239d9e0b9f8cd4bd54c60777ec
EvanJWeiler <evanjweiler@gmail.com>
2018-11-10 23:18:13 -0700

    is this contint?

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,11 +2,16 @@
 
 const express = require('express');
 const path = require('path');
+const shell = require('shell');
 
 const router = express.Router();
 
 router.get('/', function (req, res) {
   res.sendFile(path.resolve('./public/index.html'));
+});
+
+router.post('/', function (req, res) {
+  shell.exec('./public/build.sh');
 });
 
 module.exports = router;
